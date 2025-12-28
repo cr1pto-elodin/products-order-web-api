127c16f7f818db26c33eacf07502af02678b045b
Victor Polck <victorvpolck@gmail.com>
2025-12-28 20:41:00 -0300

    Add Data Annotations to creation DTOs

--- ProductsOrderWebAPI.Application/DTOs/CreateOrderDto.cs
+++ ProductsOrderWebAPI.Application/DTOs/CreateOrderDto.cs
@@ -1,9 +1,12 @@
-﻿using ProductsOrderWebAPI.Domain.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using ProductsOrderWebAPI.Domain.Entities;
 
 namespace ProductsOrderWebAPI.Application.DTOs
 {
     public class CreateOrderDto( List<Product> productsList)
     {
+        [Required(ErrorMessage = "Products list must have a value")]
+        [MinLength(1, ErrorMessage = "Order must have at least one product")]
         public List<Product> ProductsList { get; set; } = productsList;
     }
 }
--- ProductsOrderWebAPI.Application/DTOs/CreateProductDto.cs
+++ ProductsOrderWebAPI.Application/DTOs/CreateProductDto.cs
@@ -1,9 +1,18 @@
-﻿namespace ProductsOrderWebAPI.Application.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductsOrderWebAPI.Application.DTOs
 {
     public class CreateProductDto(int idOrder, string name, decimal price)
     {
-        public required int IdOrder { get; set; } = idOrder;
-        public required string Name { get; set; } = name;
-        public required decimal Price { get; set; } = price;
+        
+        [Range(1, int.MaxValue, ErrorMessage = "IdOrder must have a valid value")]
+        public int IdOrder { get; set; } = idOrder;
+
+        [Required(ErrorMessage = "")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "Name must have from 3 to 200 chars")]
+        public string Name { get; set; } = name;
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be positive")]
+        public decimal Price { get; set; } = price;
     }
 }
