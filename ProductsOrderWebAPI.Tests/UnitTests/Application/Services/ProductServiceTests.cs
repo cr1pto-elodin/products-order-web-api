dcfc4214043982d0bd360be24c111e62e7c3e40a
Victor Polck <victorvpolck@gmail.com>
2025-12-28 17:28:12 -0300

    Add first files to Unit tests

---
+++ ProductsOrderWebAPI.Tests/UnitTests/Application/Services/ProductServiceTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductsOrderWebAPI.Tests.UnitTests.Application.Services
+{
+    internal class ProductServiceTests
+    {
+    }
+}
