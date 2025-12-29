11b408feb1e5b1c62a5f0e6862de15e9e7a57b86
Victor Polck <victorvpolck@gmail.com>
2025-12-28 23:47:25 -0300

    Add trigger that tracks all the value changes

---
+++ ProductsOrderWebAPI.Infrastructure/Migrations/20251229023815_AddOrderTotalTrigger.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProductsOrderWebAPI.Infrastructure.Migrations
+{
+    public partial class AddOrderTotalTrigger : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE TRIGGER TRG_UpdateOrderTotal
+                ON ItemPedido
+                AFTER INSERT, UPDATE, DELETE
+                AS
+                BEGIN
+                    UPDATE [Pedido]
+                    SET VALOR_TOTAL = (SELECT ISNULL(SUM(Price), 0) FROM Product WHERE ID_PEDIDO = [Pedido].Id)
+                    WHERE Id IN (SELECT ID_PEDIDO FROM Product);
+                END
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP TRIGGER IF EXISTS TRG_UpdateOrderTotal");
+        }
+    }
+}
