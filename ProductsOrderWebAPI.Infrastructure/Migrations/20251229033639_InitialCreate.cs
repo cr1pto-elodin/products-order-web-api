c4c94566dbcd95a070255549055cf86c1af38b43
Victor Polck <victorvpolck@gmail.com>
2025-12-29 00:37:24 -0300

    Initial Migration

---
+++ ProductsOrderWebAPI.Infrastructure/Migrations/20251229033639_InitialCreate.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductsOrderWebAPI.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pedido",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    VALOR_TOTAL = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false, defaultValue: 0m),
+                    DATA_CRIACAO = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DATA_ATUALIZACAO = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pedido", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ItemPedido",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ID_PEDIDO = table.Column<int>(type: "int", nullable: false),
+                    NOME = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    PRECO = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    DATA_CRIACAO = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DATA_ATUALIZACAO = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemPedido", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ItemPedido_Pedido_ID_PEDIDO",
+                        column: x => x.ID_PEDIDO,
+                        principalTable: "Pedido",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemPedido_ID_PEDIDO",
+                table: "ItemPedido",
+                column: "ID_PEDIDO");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ItemPedido");
+
+            migrationBuilder.DropTable(
+                name: "Pedido");
+        }
+    }
+}
