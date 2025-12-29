e1577ce7b03916db2b3476aa8d769342f49ac225
Victor Polck <victorvpolck@gmail.com>
2025-12-29 00:02:24 -0300

    Add Swagger

--- ProductsOrderWebAPI.API/Program.cs
+++ ProductsOrderWebAPI.API/Program.cs
@@ -6,12 +6,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.MapOpenApi();
 }
 
