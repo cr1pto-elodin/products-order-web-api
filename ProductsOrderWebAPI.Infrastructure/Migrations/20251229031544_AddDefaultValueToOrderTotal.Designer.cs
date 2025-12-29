a30c7377a6eb43a646e839d26a36a34eb24f0930
Victor Polck <victorvpolck@gmail.com>
2025-12-29 00:23:40 -0300

    add default value to stop error while adding empty value to "VALOR_TOTAL"

--- ProductsOrderWebAPI.Infrastructure/Migrations/20251229031544_AddDefaultValueToOrderTotal.Designer.cs
+++ ProductsOrderWebAPI.Infrastructure/Migrations/20251229031544_AddDefaultValueToOrderTotal.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProductsOrderWebAPI.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using ProductsOrderWebAPI.Infrastructure.Data;
 namespace ProductsOrderWebAPI.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251229031544_AddDefaultValueToOrderTotal")]
+    partial class AddDefaultValueToOrderTotal
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ProductsOrderWebAPI.Infrastructure/Migrations/20251229031544_AddDefaultValueToOrderTotal.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductsOrderWebAPI.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDefaultValueToOrderTotal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "VALOR_TOTAL",
+                table: "Pedido",
+                type: "decimal(18,2)",
+                precision: 18,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldPrecision: 18,
+                oldScale: 2);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "VALOR_TOTAL",
+                table: "Pedido",
+                type: "decimal(18,2)",
+                precision: 18,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldPrecision: 18,
+                oldScale: 2,
+                oldDefaultValue: 0m);
+        }
+    }
+}
